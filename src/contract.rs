c83b2ec33d24d359c50a7321a0e9f72cf81f2905
Xavier Leprêtre <xavier.lepretre@gmail.com>
2024-08-15 22:04:10 +0400

    Add first unit test.

--- src/contract.rs
+++ src/contract.rs
@@ -12,3 +12,32 @@ pub fn instantiate(
 ) -> ContractResult {
     Ok(Response::default())
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::msg::InstantiateMsg;
+    use cosmwasm_std::{testing, Addr, Response};
+
+    #[test]
+    fn test_instantiate() {
+        // Arrange
+        let mut mocked_deps_mut = testing::mock_dependencies();
+        let mocked_env = testing::mock_env();
+        let mocked_addr = Addr::unchecked("addr");
+        let mocked_msg_info = testing::message_info(&mocked_addr, &[]);
+
+        let instantiate_msg = InstantiateMsg {};
+
+        // Act
+        let contract_result = super::instantiate(
+            mocked_deps_mut.as_mut(),
+            mocked_env,
+            mocked_msg_info,
+            instantiate_msg,
+        );
+
+        // Assert
+        assert!(contract_result.is_ok(), "Failed to instantiate");
+        assert_eq!(contract_result.unwrap(), Response::default())
+    }
+}
